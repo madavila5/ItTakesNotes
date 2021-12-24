fee3db909c40c1b5032d76cc45de3af8452cf8b8
Miguel Davila <migueldavila@Miguels-Mac-mini.local>
2021-12-23 17:48:07 -0800

    Update to server const need the app use and get

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
+const savedEntries = require('./db/db.json');
+savedEntries.forEach((element, i) => {element.id = i+1});
+fs.writeFileSync('./db/db.json', JSON.stringify(savedEntries));
+
+const app = express();
+const PORT = process.env.PORT||3002;
+
+app.use
