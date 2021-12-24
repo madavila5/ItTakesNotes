6e0092ff55d795a963a91ad1c1b5d7ad0c284c90
Miguel Davila <migueldavila@Miguels-Mac-mini.local>
2021-12-23 17:56:17 -0800

    Update to server use and get

--- server.js
+++ server.js
@@ -8,5 +8,18 @@ fs.writeFileSync('./db/db.json', JSON.stringify(savedEntries));
 
 const app = express();
 const PORT = process.env.PORT||3002;
-
-app.use
+//USE
+app.use(express.urlencoded ({extended: true}));
+app.use(express.join());
+app.use(express.static('public'));
+//GET
+app.get('/', (req,res)=>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+app.get('/notes', (req,res)=>
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+app.get('/api/notes', (req,res)=>{
+    let notes = JSON.parse(fs.readFileSync('./db/db.json', "utf-8"));
+    res.json(notes);}
+);
